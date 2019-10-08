08f0e2c8c3ac47d4d09c8a3d37b9fd66a5052fee
Jordy Koppers <jordy.koppers@student.hu.nl>
2019-10-08 13:58:54 +0200

    Get value of HTML inputs

--- SDLab/src/app/modules/pages/registerpage/registerpage.component.ts
+++ SDLab/src/app/modules/pages/registerpage/registerpage.component.ts
@@ -9,6 +9,16 @@ export class RegisterpageComponent implements OnInit {
 
   constructor() { }
 
+  sendRegistration() {
+    var studentmail = (<HTMLInputElement>document.getElementById('studentenmail')).value;
+    var studentennummer = (<HTMLInputElement>document.getElementById('studentennummer')).value;
+    var voornaam = (<HTMLInputElement>document.getElementById('voornaam')).value;
+    var achternaam = (<HTMLInputElement>document.getElementById('achternaam')).value;
+
+    // console.log('Added: ' + studentmail + ' ' + studentennummer + ' ' + voornaam + ' ' + achternaam + ' to registrationlist');
+
+  }
+
   ngOnInit() {
   }
 
