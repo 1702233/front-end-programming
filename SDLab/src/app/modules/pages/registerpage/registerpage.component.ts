5b72595d1c00a8ab387468e11c1191bd793f588e
Jordy Koppers <jordy.koppers@student.hu.nl>
2019-10-09 16:21:29 +0200

    Git broke ohoh

--- SDLab/src/app/modules/pages/registerpage/registerpage.component.ts
+++ SDLab/src/app/modules/pages/registerpage/registerpage.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { RegisterService } from 'src/app/core/services/register.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registerpage',
@@ -7,19 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private service: RegisterService,
+    private firestore: AngularFirestore,
+    private toastr: ToastrService
+  ) { }
 
-  sendRegistration() {
-    let studentmail = (<HTMLInputElement> document.getElementById('studentenmail')).value;
-    let studentennummer = (<HTMLInputElement> document.getElementById('studentennummer')).value;
-    let voornaam = (<HTMLInputElement> document.getElementById('voornaam')).value;
-    let achternaam = (<HTMLInputElement> document.getElementById('achternaam')).value;
+  resetForm(form?: NgForm) {
+    if (form != null) {
+      form.resetForm();
+    }
 
-    // console.log('Added: ' + studentmail + ' ' + studentennummer + ' ' + voornaam + ' ' + achternaam + ' to registrationlist');
+    this.service.formData = {
+      id: null,
+      studentenmail: '',
+      studentennummer: null,
+      voornaam: '',
+      achternaam: '',
+    };
+  }
 
+  onSubmit(form: NgForm) {
+    let data = Object.assign({}, form.value);
+    delete data.id;
+    if (form.value.id == null) {
+      this.firestore.collection('registraties').add(data);
+    } else {
+      this.firestore.doc('registraties/' + form.value.id).update(data);
+    }
+    this.resetForm(form);
+    this.toastr.success('submitted succesfully', 'Registratie gedaan.');
   }
 
   ngOnInit() {
+    this.resetForm();
   }
 
 }
